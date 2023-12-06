644656fa86858233b5073b86fd770bfdd1a5407b
ndatla1020 <satya.nishanth@gmail.com>
2023-12-05 21:46:18 -0500

    github pages final

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react'; import './App.css'; import HelloWorld from './HelloWorld';
+import Counter from './Counter';
+class App extends Component { render() {
+ return (
+ <div className="App">
+ <HelloWorld name={'[Nishant]'} />
+ <Counter />
+ <FilteredList items={produce}/>
+ </div>
+ );
+ }
 }
-
-export default App;
+export default App; 
