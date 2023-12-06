50d32be61d65c12573c2d9c4b9638724ac2948ca
ndatla1020 <satya.nishanth@gmail.com>
2023-12-05 21:48:36 -0500

    github pages final

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
 import React, { Component } from 'react'; import './App.css'; import HelloWorld from './HelloWorld';
 import Counter from './Counter';
+import FilteredList from './FilteredList';
+
+const produce =
+[
+ {name: "Apple", type: "Fruit"},
+ {name: "Pineapple", type: "Fruit"} ,
+ {name: "Banana", type: "Fruit"},
+ {name: "Pear", type: "Fruit"},
+ {name: "Strawberry", type: "Fruit"},
+ {name: "Orange", type: "Fruit"},
+ {name: "Lettuce", type: "Vegetable"},
+ {name: "Cucumber", type: "Vegetable"},
+ {name: "Eggplant", type: "Vegetable"},
+ {name: "Squash", type: "Vegetable"},
+ {name: "Bell pepper", type: "Vegetable"},
+ {name: "Onion", type: "Vegetable"},
+];
+
 class App extends Component { render() {
  return (
  <div className="App">
